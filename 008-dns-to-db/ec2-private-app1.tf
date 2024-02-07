e272b5195aaa177e8f0615ad2ecb1d96c3a84598
Amal Kuriakose <amalkuriakose802@gmail.com>
2024-02-07 20:16:24 +0530

    Added config files for dns to db

---
+++ 008-dns-to-db/ec2-private-app1.tf
@@ -0,0 +1,15 @@
+module "ec2_private_app1" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.0"
+
+  name                   = "${var.environment}-app1"
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  vpc_security_group_ids = [module.private_sg.security_group_id]
+  for_each               = toset(["0", "1"])
+  subnet_id              = element(module.vpc.private_subnets, tonumber(each.key))
+  user_data              = file("${path.module}/app1-install.sh")
+  tags                   = local.common_tags
+  depends_on             = [module.vpc]
+}
